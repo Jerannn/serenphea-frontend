60da86b1f4a14a5008dc19645e5effe173a6f9cd
Jerannn <jerandeduro16@gmail.com>
2026-05-18 12:51:42 +0800

    feat(property): add amenity removal tracking for property updates

--- src/features/host/properties/actions/amenities-action.ts
+++ src/features/host/properties/actions/amenities-action.ts
@@ -1,16 +1,21 @@
 import { api } from "@/lib/api";
 import { redirect, type ActionFunctionArgs } from "react-router-dom";
 import { usePropertyStore } from "../store/PropertyStore";
-import type { ErrorResponse } from "@/shared/types/response-types";
+import type {
+  ErrorResponse,
+  SuccessResponse,
+} from "@/shared/types/response-types";
+import type { Amenity } from "../types";
 
 export default async function action({ request, params }: ActionFunctionArgs) {
   const data = await request.json();
   const propertyId = params.id;
+  console.log(data);
 
-  const response = await api(`/properties/${propertyId}/amenities`, {
+  const response = (await api(`/properties/${propertyId}/amenities`, {
     method: "PUT",
     body: JSON.stringify(data),
-  });
+  })) as SuccessResponse<{ amenities: Amenity[] }> | ErrorResponse;
 
   if (response.status === "fail") {
     return response as ErrorResponse;
